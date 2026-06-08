9088160c9f83993cea967402f24be6a544ff0d70
Harsh Mishra <85541359+HarshMishra2007@users.noreply.github.com>
2026-06-08 12:48:44 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
+# First Python program by Harsh Mishra
+
 print("Hello, I am Harsh Mishra")
-print("I am starting my coding journey")
-print("I will become a software engineer")
+print("I am starting my coding journey in Python")
+
+name = "Harsh Mishra"
+goal = "Software Engineer"
+
+print("Name:", name)
+print("Goal:", goal)
+
+for i in range(1, 6):
+    print("Learning step", i)
